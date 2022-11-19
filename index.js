f40bebb1100d17209865addbc97ccf746657f40d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-11-20 06:28:57 +0700

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const serveIndex = require("serve-index");
+app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(
+  "/",
+  express.static("public"),
+  serveIndex("public", { icons: true, stylesheet: "./styles/custom.css" })
+);
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html', { root: __dirname + '/public' });
+// });
+app.listen(3000);
