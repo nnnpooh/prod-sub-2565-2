bc0768eb5fd03eca5b8a5b36c4a1ae1b27d0b051
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-11-21 13:44:47 +0700

    Use Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  content: ['**.{html,js,css}'],
   theme: {
     extend: {},
   },
