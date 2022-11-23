26566d432dad535d1aa0ffcb8a26bf96604b069e
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-11-23 17:31:35 +0700

    update

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 var lecturePath = path.join(__dirname, "lectures", "src");
+var flowPath = path.join(__dirname, "flows");
 app.use(
   "/lectures",
   express.static(lecturePath),
   serveIndex(lecturePath, { icons: true, stylesheet: "./styles/custom.css" })
 );
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname + '/public' });
+app.use(
+  "/flows",
+  express.static(flowPath),
+  serveIndex(flowPath, { icons: true, stylesheet: "./styles/custom.css" })
+);
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/public" });
 });
 
 var port = process.env.PORT || 3000;
